3a9c9e97f11aaacfe11423f22b41c9f2dd0fd522
jfk1987 <1987jfk@googlemail.com>
2021-11-17 18:02:03 +0100

    Added test class for client

---
+++ CloudFlareApiClientTest/ClientTest.cs
@@ -0,0 +1,24 @@
+﻿using CloudFlareApiClient;
+using Xunit;
+
+namespace CloudFlareApiClientTest
+{
+    public class ClientTest
+    {
+        [Fact]
+        public void ContructorTest()
+        {
+            Configuration config01 = new Configuration("", "", "");
+            Client client01 = new Client(config01);
+
+            Assert.Equal(config01, client01.GetConfiguration());
+
+            Configuration config02 = new Configuration("", "");
+            Client client02 = new Client(config02);
+
+            Assert.Equal(config02, client02.GetConfiguration());
+
+            Assert.NotEqual(client01.GetConfiguration(), client02.GetConfiguration());
+        }
+    }
+}
