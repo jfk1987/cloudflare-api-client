7c03bf6e553b4cf5526f9d448140b214eecf7a7c
jfk1987 <1987jfk@googlemail.com>
2021-11-30 21:33:05 +0100

    Added empty list to avoid null errors

--- CloudFlareApiClient/RestRequest.cs
+++ CloudFlareApiClient/RestRequest.cs
@@ -9,5 +9,10 @@ namespace CloudFlareApiClient
         public string Path { get; set; }
         public List<UrlParameter> UrlParameters { get; set; }
         public string Body { get; set; }
+
+        public RestRequest()
+        {
+            UrlParameters = new List<UrlParameter>();
+        }
     }
 }
