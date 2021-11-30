ed9a96d2fc713a4b1b28a532345fdb07c5e98d7a
jfk1987 <1987jfk@googlemail.com>
2021-11-30 20:46:39 +0100

    Added UrlParameter class

--- CloudFlareApiClient/RestRequest.cs
+++ CloudFlareApiClient/RestRequest.cs
@@ -7,7 +7,7 @@ namespace CloudFlareApiClient
     public class RestRequest
     {
         public string path;
-        public List<Dictionary<string, string>> UrlParameters { get; set; }
+        public List<UrlParameter> UrlParameters { get; set; }
         public string Body { get; set; }
     }
 }
