5558249807ea9766193f2ede1d550c1dad55396c
jfk1987 <1987jfk@googlemail.com>
2021-12-02 20:28:56 +0100

    Client cleanup

--- CloudFlareApiClient/Client.cs
+++ CloudFlareApiClient/Client.cs
@@ -11,7 +11,6 @@ namespace CloudFlareApiClient
     {
         private readonly RestClient _client;
         private readonly Configuration _configuration;
-        private const string BASEURL = "https://api.cloudflare.com/client/v4";
 
         public Client(Configuration configuration)
         {
@@ -19,59 +18,41 @@ namespace CloudFlareApiClient
             _client = new RestClient(configuration);
         }
 
-        public Client(Configuration configuration, RestClient restClient)
-        {
-            _client = restClient;
-        }
         public Configuration GetConfiguration()
         {
             return _configuration;
         }
 
-        public async void UpdateDnsRecord()
+        public async Task GetDnsZonesAsync()
         {
-            
-
-            DnsRecordRequest request = new DnsRecordRequest
+            var request = new RestRequest
             {
-                Content = "",
-                Name = "",
-                RecordType = DnsRecordType.A,
-                Proxied = true,
+                Path = "/zones"
             };
 
-            SendRequest(HttpMethod.PATCH, $"/zones/{_configuration.ZoneId}/dns_records");               
+            var response = await _client.GetRequestAsync(request);
         }
 
-        public async Task<string> GetRecords()
-        {
-            var response = await SendRequest(HttpMethod.GET, $"/zones/{_configuration.ZoneId}/dns_records");
-
-            return await response.Content.ReadAsStringAsync();
-        }
-
-        public async Task<HttpResponseMessage> SendRequest(HttpMethod method, string path, string content = null)
-        {
-            HttpClient client = new HttpClient();
-
-            string requestPath = BASEURL + path;
-
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _configuration.ApiToken);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            HttpResponseMessage response;       
+        //public async void UpdateDnsRecord()
+        //{
             
-            switch(method)
-            {
-                case HttpMethod.GET: response = await client.GetAsync(requestPath); break;
-                case HttpMethod.POST: response = await client.PostAsync(requestPath, null); break;
-                case HttpMethod.PATCH: response = await client.PatchAsync(requestPath, null); break;
-                case HttpMethod.PUT: response = await client.PutAsync(requestPath, null); break;
-                case HttpMethod.DELETE: response = await client.DeleteAsync(requestPath); break;
-                default: throw new ArgumentException("Unimplemented HTTP method!");
-            }
 
-            return response;
-        }
+        //    DnsRecordRequest request = new DnsRecordRequest
+        //    {
+        //        Content = "",
+        //        Name = "",
+        //        RecordType = DnsRecordType.A,
+        //        Proxied = true,
+        //    };
+
+        //    SendRequest(HttpMethod.PATCH, $"/zones/{_configuration.ZoneId}/dns_records");               
+        //}
+
+        //public async Task<string> GetRecords()
+        //{
+        //    var response = await SendRequest(HttpMethod.GET, $"/zones/{_configuration.ZoneId}/dns_records");
+
+        //    return await response.Content.ReadAsStringAsync();
+        //}
     }
 }
